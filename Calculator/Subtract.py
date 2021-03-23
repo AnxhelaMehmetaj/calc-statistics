26a60e04679d9fcdb667f0651a735a248a479f4c
Anxhela Mehmetaj <anxhela.mehmetaj@hotmail.com>
2021-03-23 14:52:46 -0400

    changes

--- Calculator/Subtract.py
+++
@@ -1,5 +0,0 @@
-def subtract(a, b):
-    a = float(a)
-    b = float(b)
-    c = b - a
-    return c
---
+++ tests/test_calculator.py
@@ -0,0 +1,104 @@
+import unittest
+
+# the .py file name is Calculator and the class name is also Calculator
+
+# Move Section to fileReader.py
+from src.calculator import Calculator
+from CSV_Reader.CSVReader import readCSV
+
+
+'''
+This is the TestCase class that tests Calculator class functions.
+'''
+
+
+class TestCalculator(unittest.TestCase):
+    # this is the Calculator class instance.
+    calculator = None
+
+    # class level setup function, execute once only before any tests function.
+    @classmethod
+    def setUpClass(cls):
+        print('')
+        print('setUpClass')
+
+    # class level setup function, execute once only after all tests function's execution.
+    @classmethod
+    def tearDownClass(cls):
+        print('')
+        print('tearDownClass')
+
+    # execute before every tests case function run.
+    def setUp(self):
+        self.calculator = Calculator()
+        print('')
+        print('setUp')
+
+    # execute after every tests case function run.
+    def tearDown(self):
+        # release the Calculator object.
+        if self.calculator is not None:
+            self.calculator = None
+        print('')
+        print('tearDown')
+
+    # below are function that tests Calculator class's plus, minus, multiple and divide functions.
+    def testInstantiateCalculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def testAddition(self):
+        rows = readCSV("tests/test_cases/Addition.csv")
+        for row in rows:
+            result = float(row["Result"])
+            x = float(row["Value 1"])
+            y = float(row["Value 2"])
+            self.assertEqual(self.calculator.add(x, y), result)
+
+    def testSubtraction(self):
+        rows = readCSV("tests/test_cases/Subtraction.csv")
+        for row in rows:
+            result = float(row["Result"])
+            y = float(row["Value 1"])
+            x = float(row["Value 2"])
+            self.assertEqual(self.calculator.subtract(x, y), result)
+
+    def testDivision(self):
+        self.assertRaises(ZeroDivisionError, self.calculator.divide, 1, 0)
+        rows = readCSV("tests/test_cases/Division.csv")
+        for row in rows:
+            result = float(row["Result"])
+            y = float(row["Value 1"])
+            x = float(row["Value 2"])
+            self.assertAlmostEqual(self.calculator.divide(x, y), result)
+
+    def testMultiplication(self):
+        rows = readCSV("tests/test_cases/Multiplication.csv")
+        for row in rows:
+            result = float(row["Result"])
+            x = float(row["Value 1"])
+            y = float(row["Value 2"])
+            self.assertAlmostEqual(self.calculator.multiply(x, y), result)
+
+    def testSquare(self):
+        rows = readCSV("tests/test_cases/Square.csv")
+        for row in rows:
+            result = float(row["Result"])
+            x = float(row["Value 1"])
+            self.assertAlmostEqual(self.calculator.square(x), result)
+
+    def testSquareRoot(self):
+        rows = readCSV("tests/test_cases/SquareRoot.csv")
+        for row in rows:
+            result = float(row["Result"])
+            x = float(row["Value 1"])
+            self.assertAlmostEqual(self.calculator.squareRoot(x), result)
+
+    def testResultProperty(self):
+        self.calculator.results.clear()
+        self.assertEqual(self.calculator.results, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
